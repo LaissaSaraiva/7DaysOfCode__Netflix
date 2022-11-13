66fd9d9f723a769c5bb9f10ac9ce72660cb74863
Laissa Saraiva <laissa_saraiva@hotmail.com>
2022-11-13 20:36:38 -0300

    Criação de Menu Mobile + Ajustes na Responsividade
    - Inclusão de botões do menu mobile ( aberto e fechado);
    
    - Criação do JavaScript para a lógica do menu mobile;
    
    Inclusão de @media (max-width: 1024px) para melhor responsividade da página.
    
    #7daysofcode

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const btnAberto = document.querySelector("#btn__aberto");
+const btnFechado = document.querySelector("#btn__fechado");
+
+// function alternarMenu(evento) {
+//   if(evento.type === "touchstart" ) {
+//     evento.preventEventDefault();
+//   }
+//   const header = document.querySelector("header");
+//   header.classList.toggle("ativo");
+// }
+
+function alternarMenu(evento) {
+  if(evento.type === "touchstart" ) {
+    evento.preventEventDefault();
+  }
+  const body = document.querySelector("body");
+  body.classList.toggle("ativo");
+}
+
+btnAberto.addEventListener("click", alternarMenu);
+btnFechado.addEventListener("click", alternarMenu);
+
+btnAberto.addEventListener("touchstart", alternarMenu);
+btnFechado.addEventListener("touchstart", alternarMenu);
